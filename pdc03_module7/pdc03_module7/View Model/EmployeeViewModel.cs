61f0781cb79778eb9c015c2613a9e56ae102252e
E11\User <caloy0714@gmail.com>
2023-06-02 12:08:00 +0800

    first commit

---
+++ pdc03_module7/pdc03_module7/View Model/EmployeeViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pdc03_module7.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace pdc03_module7.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        //Call Database
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+
+        //Insert records
+        public int InsertEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        //Retrieve records
+        public async Task<List<EmployeeModel>> GetAllEmployees()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Employees.ToListAsync();
+            return res;
+
+        }
+
+        //Delete Records
+        public int DeleteEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        //Update records
+        public async Task<int> UpdateEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Update(obj);
+            int c = await _dbContext.SaveChangesAsync();
+            return c;
+        }
+
+
+    }
+}
